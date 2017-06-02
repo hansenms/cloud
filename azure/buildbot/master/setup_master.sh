e08c815d4bc1d63f1783c9f6f1274b04fe38bf55
Michael S. Hansen <michael.hansen@nih.gov>
2017-06-02 10:19:40 -0400

    Fixed permissions on buildbot master folder.

--- azure/buildbot/master/setup_master.sh
+++ azure/buildbot/master/setup_master.sh
@@ -8,5 +8,7 @@ pip install 'buildbot[bundle]'
 buildbot create-master /home/buildbot/master
 cp /home/buildbot/master/master.cfg.sample /home/buildbot/master/master.cfg
 
+chown -R buildbot:buildbot /home/buildbot/master
+
 cp buildbot-master.service /etc/systemd/system/
 service buildbot-master start
