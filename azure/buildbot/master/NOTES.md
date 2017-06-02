e5e1d111c88cebdf7405375d246e3a03d0cba674
Michael S. Hansen <michael.hansen@nih.gov>
2017-06-02 09:03:14 -0400

    Added Azure buildbot master configuration

---
+++ azure/buildbot/master/NOTES.md
@@ -0,0 +1,14 @@
+Notes Master configuration
+===========================
+
+TODO
+-----
+
+* Authentication
+* All builders
+* Move data to Azure
+
+GitHub Authentication
+---------------------
+
+* Should be set up
---
+++ azure/buildbot/master/setup_master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y install python-pip
+pip install --upgrade pip
+pip install 'buildbot[bundle]'
+
+buildbot create-master /home/buildbot/master
+cp /home/buildbot/master/master.cfg.example /home/buildbot/master/master.cfg
+
+cp buildbot-master.service /etc/systemd/system/
+service buildbot-master start
