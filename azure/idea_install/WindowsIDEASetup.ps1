7f6c7814d3fdf5932bfbb8a16ca8b868b54a2b40
Michael S. Hansen <michael.hansen@nih.gov>
2017-06-23 14:17:55 -0400

    idea_install: Removed WinCDEmu, causes problems for automated setup.

--- azure/idea_install/WindowsIDEASetup.ps1
+++ azure/idea_install/WindowsIDEASetup.ps1
@@ -5,7 +5,6 @@ choco install -y imdisk
 choco install -y git
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 choco install -y googlechrome
-choco install -y wincdemu
 
 Import-Module ServerManager
 Add-WindowsFeature as-net-framework
