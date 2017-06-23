485ecd31369ab71f620738174c94521f862756f0
Michael S. Hansen <michael.hansen@nih.gov>
2017-06-23 10:07:56 -0400

    idea_install: removed changes to IE, since we are installing chrome.

--- azure/idea_install/WindowsIDEASetup.ps1
+++ azure/idea_install/WindowsIDEASetup.ps1
@@ -1,31 +1,3 @@
-function Disable-InternetExplorerESC {
-
-    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
-    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
-    Stop-Process -Name Explorer -Force
-    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
-}
-
-
-function Enable-InternetExplorerESC {
-    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 1 -Force
-    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 1 -Force
-    Stop-Process -Name Explorer
-    Write-Host "IE Enhanced Security Configuration (ESC) has been enabled." -ForegroundColor Green
-}
-
-
-function Disable-UserAccessControl {
-    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000 -Force
-    Write-Host "User Access Control (UAC) has been disabled." -ForegroundColor Green
-}
-
-
-
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y wget
@@ -34,10 +6,6 @@ choco install -y git
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 choco install -y googlechrome
 choco install -y wincdemu
-
-Disable-UserAccessControl
-
-Disable-InternetExplorerESC
 
 Import-Module ServerManager
 Add-WindowsFeature as-net-framework
